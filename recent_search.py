c0b2b76d9c6feee1665d7fe7114b5b28d07aec28
Aléxis Toigo <alexis@alexis.toigo>
2022-02-14 14:40:24 -0300

    Initial commit

---
+++ recent_search.py
@@ -0,0 +1,55 @@
+import requests
+import os
+import json
+
+
+# To set your enviornment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+
+
+def auth():
+    return os.environ.get("BEARER_TOKEN")
+    # return os.environ.get("AAAAAAAAAAAAAAAAAAAAAIqqZAEAAAAAB6tm0rEAT3si%2FbEuYHCo1tRuSKc%3D0vVnNMTttI8CfkYx03bWhiWUKc40btSUezFyfPDz70zEE3nwPQ")
+
+
+def create_url():
+    query = "Neymar"
+    # Tweet fields are adjustable.
+    # Options include:
+    # attachments, author_id, context_annotations,
+    # conversation_id, created_at, entities, geo, id,
+    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
+    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
+    # source, text, and withheld
+    tweet_fields = "tweet.fields=author_id,conversation_id,created_at,id,in_reply_to_user_id,public_metrics,text"
+    user_fields = "expansions=author_id&user.fields=id,name,username,created_at"
+    filters = "start_time=2022-02-10T00:00:00.00Z&end_time=2022-02-13T00:00:00.00Z"
+    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}&{}&{}".format(
+        query, tweet_fields, user_fields, filters
+    )
+    return url
+
+
+def create_headers(bearer_token):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
+
+
+def connect_to_endpoint(url, headers):
+    response = requests.request("GET", url, headers=headers)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+
+def main():
+    bearer_token = auth()
+    url = create_url()
+    headers = create_headers(bearer_token)
+    json_response = connect_to_endpoint(url, headers)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
